3d9cb2a129280ed9639c423bc43582663b779ebc
Kenny Elshoff <github@kennyelshoff.com>
2023-07-28 23:12:17 -0400

    fix: dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Button>hello</Button>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    //   <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    //     <Button>hello</Button>
+    //   </div>
+    // </main>
+    <section className="bg-background container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <Button>Hello</Button>
       </div>
-    </main>
+    </section>
   );
 }
