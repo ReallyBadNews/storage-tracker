9e0bdade5d4d206d3263f33c03655cd6cb5a82c5
Kenny Elshoff <github@kennyelshoff.com>
2023-07-30 23:05:30 -0400

    chore(deps): upgrade packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section className="bg-background container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <Button>Hello</Button>
+        <Button>Hello world</Button>
       </div>
     </section>
   );
