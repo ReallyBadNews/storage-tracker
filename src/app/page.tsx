027a734a70eed91c04f651bebc9f424154bf16b6
Kenny Elshoff <github@kennyelshoff.com>
2023-08-12 23:18:42 -0400

    base form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <section className="bg-background container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <Button>Hello world</Button>
+        <Link className={buttonVariants({ variant: "outline" })} href="/login">
+          Log in
+        </Link>
       </div>
     </section>
   );
