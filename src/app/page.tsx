fd31995391665c42a44c415f2cddb2ad77404042
Kenny Elshoff <github@kennyelshoff.com>
2023-09-09 23:31:08 -0400

    feat: schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { LoginButton } from "@/components/login-button";
-
 export default function Home() {
   return (
     <section className="bg-background container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <LoginButton>Log in</LoginButton>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Hello world!
+        </h1>
       </div>
     </section>
   );
