67ad6012155191d223dbd817ba0bb4102b1909b9
Kenny Elshoff <github@kennyelshoff.com>
2023-10-01 22:26:12 -0400

    wip: signup layout

--- src/app/authentication/layout.tsx
+++ src/app/authentication/layout.tsx
@@ -1,12 +1,29 @@
+import { siteConfig } from "@/config/site";
+import Link from "next/link";
+
 export default function AuthLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <div>
-      <h1>hello from authlayout</h1>
-      {children}
-    </div>
-  )
+    <>
+      <div className="relative grid min-h-[calc(100vh-65px)] grid-cols-1 overflow-hidden md:grid-cols-3 lg:grid-cols-2">
+        <div className="relative">
+          <div
+            className="absolute inset-0 bg-cover"
+            style={{
+              backgroundImage:
+                "url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80)",
+            }}
+          />
+          <div className="absolute inset-0 bg-gradient-to-t from-background to-background/60 md:to-background/40" />
+        </div>
+
+        <div className="container absolute top-1/2 col-span-1 flex -translate-y-1/2 items-center md:static md:top-0 md:col-span-2 md:flex md:translate-y-0 lg:col-span-1">
+          {children}
+        </div>
+      </div>
+    </>
+  );
 }
